3283f7db420fd414474bdee8127b569a4440b8c2
Ihor <kovaluk_igor@ukr.net>
2023-12-28 16:04:21 +0100

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const refGallery = document.querySelector(".gallery");
+
+const markupGallery = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <div class="gallery">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt=""  title="${description}" />
+      </a>
+    </div>
+</li>
+`}).join("");
+
+refGallery.innerHTML = markupGallery;
+
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
